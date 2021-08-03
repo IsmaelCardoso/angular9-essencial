7d3facd68fad32a1cc8de61a0bd30c53d28a2a29
IsmaelCardoso <ismael.cardosos@gmail.com>
2021-08-03 17:09:46 -0300

    chor(home.component): created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,16 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +30,7 @@ import { MatListModule } from "@angular/material/list";
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
